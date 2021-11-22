ec5942847b41ad083f23f381ab3ab7b7b4052108
JC Renner <jc-renner@lu-tec.com>
2021-11-22 00:56:52 -0500

    header image, google fonts, colors

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import coverImage from '../../assets/cover/cover-image.jpg';
+import coverImage from '../../assets/cover/cover-image-4.png';
 
 function Header(props) {
 
   return (
     <header className="flex-row space-between px-1">
-      <h1>Lernantino</h1>
-      <img src={coverImage} alt="wooden background"></img>
+      <h1 className="pl-5">JC-Renner</h1>
+      <img src={coverImage} alt="pixelated digital background"></img>
       {props.children}
     </header>
   );
